3032991c29044efb98f9831322239656ed2b3f24
Victor Ramos <victorpramos19@gmail.com>
2025-05-18 17:01:46 -0300

    searchBar styles
    matei o arquivo .css e add as linhas no .scss

--- web/components/SearchBar.jsx
+++ web/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import '../styles/SearchBar.css';
 
 function SearchBar() {
   const navigate = useNavigate();

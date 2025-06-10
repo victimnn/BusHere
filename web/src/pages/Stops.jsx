5cb2957d949409ae9847a84a7bd2b2bb42b65179
Laggh <77841497+Laggh@users.noreply.github.com>
2025-06-10 12:14:05 -0300

    usando o componente do mapa

--- web/src/pages/Stops.jsx
+++ web/src/pages/Stops.jsx
@@ -1,22 +1,29 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import PopUpComponent from "../components/PopUpComponent";
+import MapComponent from "../components/MapComponent";
 
 function PopUpContent() {
   return (
-    <div className="p-3 bg-blue">
+    <div>
       <h2>Conteúdo do PopUp</h2>
-      <p>Essa é a page dos pontos.</p>
+      <p>Este é o conteúdo do PopUp.</p>
     </div>
   );
 }
 
 function Stops(){
+    //TODO(): Isso é para teste
+    const [size, setSize] = useState(32); // Estado para o tamanho do ícone
+    const handleMapClick = (latlng) => {
+      alert(`Você clicou em: ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)}`);
+    }
+
     const popUpRef = useRef(null); // Referência para o componente PopUpComponent
   
     return (
       <main>
         <h1>Pontos</h1> 
-  
+    
         <button
           onClick={() => {
             popUpRef.current.show(PopUpContent, {}, "PopUp dos pontos"); // Chama a função show do PopUpComponent, sempre 3 parametos
@@ -28,9 +35,16 @@ function Stops(){
           
         {/* texto com a cor secundaria */}
         <p className="text-secondary">Texto com a cor secundaria</p> 
-
-
-  
+        
+        <MapComponent 
+          className="w-100 h-100 rounded-3"
+          center={[-22.698, -47.009]}
+          zoom={13}
+          markers={[{ position: [-22.698, -47.009], popupContent: (<PopUpComponent/>), color: 'red', size: size }]}
+          polylines={[{ positions: [[-22.698, -47.009], [-22.700, -47.010]], color: 'blue' }]}
+          onMapClick={handleMapClick} // Passa a função de clique no mapa
+        />
+        
         <PopUpComponent 
           ref={popUpRef}
         />

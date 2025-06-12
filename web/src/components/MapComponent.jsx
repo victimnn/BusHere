b42195ce4c489cd4928af546b7388a5a50591b52
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-06-12 17:21:46 -0300

    fazendo componente do mapa ter multiplas alturas

--- web/src/components/MapComponent.jsx
+++ web/src/components/MapComponent.jsx
@@ -28,7 +28,7 @@ function MapComponent({ center, zoom = 13, markers = [], polylines = [], onMapCl
 
   return (
     <div className={`map-container-wrapper ${className}`}> {/* Um wrapper para controlar o tamanho */}
-      <MapContainer center={center} zoom={zoom} style={{ height: '300px', width: '100%' }}>
+      <MapContainer center={center} zoom={zoom} className="w-100 h-100">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

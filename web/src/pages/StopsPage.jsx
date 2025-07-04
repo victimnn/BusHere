d60fee8a0f1ac0c722726d48ebf1b7928a0e5a54
Victor Ramos <victorpramos19@gmail.com>
2025-07-04 00:47:20 -0300

    Add StopDetails component and integrate with StopsPage
    Introduces the StopDetails component for displaying detailed stop information using a new stopConfig in detailConfigs.js. Updates StopsPage to show StopDetails in a popup when a table row is clicked, improving the user experience for viewing stop details.

--- web/src/pages/StopsPage.jsx
+++ web/src/pages/StopsPage.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import PopUpComponent from "../components/PopUpComponent";
+import StopDetails from "../components/stops/StopDetails";
 import MapComponent from "../components/MapComponent";
 import Table from "../components/Table";
 import api from "../api/api";
@@ -173,14 +174,36 @@ function Stops({ pageFunctions }) {
   }, []); // só busca/move quando o mapa estiver pronto
 
 
-  const handleRowClick = (rowData) => {
-    const marker = markers.find(m => m.id === rowData.id);
-    if (marker) {
-      popUpRef.current.show(() => marker.popupContent, {}, "Parada Detalhes");
-    } else {
-      console.error("Marcador não encontrado para a parada clicada:", rowData.id);
+  // const handleRowClick = (rowData) => {
+  //   const marker = markers.find(m => m.id === rowData.id);
+  //   if (marker) {
+  //     popUpRef.current.show(() => marker.popupContent, {}, "Parada Detalhes");
+  //   } else {
+  //     console.error("Marcador não encontrado para a parada clicada:", rowData.id);
+  //   }
+  // }
+
+  // Handler para quando uma linha for clicada
+    const handleRowClick = (rowData) => {
+      // Encontrar o objeto stop original baseado no ID da linha clicada
+      const originalStop = stops.find(stop => stop.ponto_id === rowData.id);
+      
+      if (originalStop) {
+        popUpRef.current.show(
+          () => (
+            <StopDetails 
+              stop={originalStop} 
+              // onEdit={handleEditStop} 
+              // onDelete={handleDeleteStop} 
+            />
+          ), 
+          {}, 
+          `Parada: ${originalStop.nome}`
+        );
+      } else {
+        console.error("Stop original não encontrado para ID:", rowData.id);
+      }
     }
-  }
 
   const handleMapClick = (latlng) => {
     //alert(`Você clicou em: ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)}`);

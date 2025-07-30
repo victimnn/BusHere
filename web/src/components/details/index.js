ce919c55da8be38a925679e6cbe68ff59df09a6e
Victor Ramos <126915227+victimnn@users.noreply.github.com>
2025-07-30 02:30:55 -0300

    exportando componentes

---
+++ web/src/components/details/index.js
@@ -0,0 +1,9 @@
+// Componentes reutilizáveis para páginas de detalhes
+export { default as DetailPage } from './DetailPage';
+export { default as DetailHeader } from './DetailHeader';
+export { default as DetailSection } from './DetailSection';
+export { default as DetailItem } from './DetailItem';
+export { default as DetailActions } from './DetailActions';
+export { default as DetailContainer } from './DetailContainer';
+export { default as DetailDebug } from './DetailDebug';
+export { CenteredCard } from './DetailPage';

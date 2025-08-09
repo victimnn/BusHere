faf6c26c15b8accf2d1f1a77bb6736e3a181e32b
Victor Ramos <126915227+victimnn@users.noreply.github.com>
2025-08-09 19:12:34 -0300

    adicionando on cascade

--- server/migrations/017_create_table_onibus_rota.sql
+++ server/migrations/017_create_table_onibus_rota.sql
@@ -12,9 +12,9 @@ CREATE TABLE OnibusRota (
     criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     ativo BOOLEAN DEFAULT TRUE,
-    FOREIGN KEY (rota_id) REFERENCES Rotas(rota_id),
-    FOREIGN KEY (onibus_id) REFERENCES Onibus(onibus_id),
-    FOREIGN KEY (motorista_id) REFERENCES Motoristas(motorista_id),
+    FOREIGN KEY (rota_id) REFERENCES Rotas(rota_id) ON DELETE CASCADE,
+    FOREIGN KEY (onibus_id) REFERENCES Onibus(onibus_id) ON DELETE CASCADE,
+    FOREIGN KEY (motorista_id) REFERENCES Motoristas(motorista_id) ON DELETE CASCADE,
 
     INDEX idx_rota_id (rota_id),
     INDEX idx_onibus_id (onibus_id),

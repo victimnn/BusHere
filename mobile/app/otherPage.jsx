c2dbf81fdeb685f90383cf63bdbe46e68126d336
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-08-12 19:24:02 -0300

    testando segunda pagina

---
+++ mobile/app/otherPage.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+function otherPage() {
+  return (
+    <View>
+      <Text>This is the other page</Text>
+    </View>
+  );
+}
+
+export default otherPage;
+    

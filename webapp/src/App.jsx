3072350ebeb99e354e839f864b5d9164de377bc1
Victor Ramos <126915227+victimnn@users.noreply.github.com>
2025-08-18 19:26:52 -0300

    arrumando pastas

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/logo.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from 'react';
+
+// Importe os componentes necessários
+import HomePage from "./pages/HomePage";
+// Adicione outros imports de páginas conforme forem criadas
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="app-mobile-container">
+        <main className="app-mobile-main">
+          <Routes>
+            <Route path="/" index element={<HomePage />} />
+
+            {/* Adicione outras rotas conforme necessário */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;

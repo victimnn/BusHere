63cbec5e84481e1869accac90e06500674e4f359
foreverr <luizgustavodesouza001@gmail.com>
2025-08-19 10:48:12 -0300

    Dark-Mode Basic

--- web/src/pages/LoginPage.jsx
+++ web/src/pages/LoginPage.jsx
@@ -53,7 +53,7 @@ function Login({pageFunctions}){
 
     return (
     <div className="login-form d-flex justify-content-center align-items-center min-vh-100 bg-success bg-gradient">
-      <div className="bg-dark text-white p-5 shadow" style={{ width: "100%", maxWidth: "400px", borderRadius: '15px'}}>
+      <div className="bg-medium p-5 shadow" style={{ width: "100%", maxWidth: "450px", borderRadius: '15px'}}>
         <h2>Olá,</h2>
         <h1>
           <strong>BEM-VINDO</strong> ao <span className="text-success">BusHere!</span>
@@ -85,16 +85,17 @@ function Login({pageFunctions}){
               className="form-control"
               placeholder="Insira a senha"
             />
+             <button>teste</button>
           </div>
 
           <div className="d-flex justify-content-between align-items-center mb-3">
             <div className="form-check">
               <input className="form-check-input" type="checkbox" role="switch" id="rememberMe"/>
-              <label className="form-check-label" htmlFor="rememberMe">
+              <label className="form-check-label" htmlFor="rememberMe"  style={{marginLeft: "5px", marginTop: "5px"}}>
                 Lembrar de mim
               </label>
             </div>
-            <a href="#" className="text-white text-decoration-underline small">
+            <a href="#" className=" text-decoration-underline small">
               Esqueceu a senha?
             </a>
           </div>
@@ -104,7 +105,7 @@ function Login({pageFunctions}){
           </button>
 
           <div className="text-center mt-3">
-            <a href="/register" className="text-white text-decoration-underline small">
+            <a href="/register" className=" text-decoration-underline small">
               Não Possui Cadastro?
             </a>
           </div>

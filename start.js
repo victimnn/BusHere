1161dd025848606fec50574c7a25bfbf4c38b49a
Laggh <77841497+Laggh@users.noreply.github.com>
2025-08-26 11:07:28 -0300

    npm start melhor

---
+++ start.js
@@ -0,0 +1,37 @@
+// Script para rodar o projeto, escolhendo quais partes do monorepo iniciar
+import { input, checkbox } from '@inquirer/prompts';
+import { exec } from 'child_process';
+import concurrently from 'concurrently';
+
+const services = [
+    { name: "Web", command: "npm run start --prefix web", color: "blue", tag:"W" },
+    { name: "Server", command: "npm run start --prefix server", color: "green", tag:"S" },
+    { name: "WebApp", command: "npm run start --prefix webapp", color: "yellow", tag:"WA" },
+    { name: "Mobile", command: "npm run start --prefix mobile", color: "red", tag:"M" }
+    //{ name: "Abrir Navegador"}
+]
+
+const checkboxOptions = services.map(service => ({
+    name: service.name,
+    value: {
+        command: service.command,
+        prefixColor: service.color,
+        name: service.tag
+    }
+}));
+
+const answer = await checkbox({
+    message: "Quais serviços você deseja iniciar?",
+    choices: checkboxOptions,
+    required: true,
+    loop: true,
+});
+
+const result = concurrently(answer);
+
+try {
+  await result.result;
+  console.log('Todos os serviços iniciados!');
+} catch (err) {
+  console.error('Erro ao iniciar serviços:', err);
+}

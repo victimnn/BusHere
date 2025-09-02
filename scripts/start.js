f5133a50caadf571d0ad78546451b96b5dc82661
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-09-01 22:49:29 -0300

    npm run start aceita nodemon agora

--- scripts/start.js
+++ scripts/start.js
@@ -4,9 +4,11 @@ import { exec } from 'child_process';
 import concurrently from 'concurrently';
 import chalk from 'chalk';
 
+
 const services = [
     { name: "Web",    command: "npm run start --prefix web",   color: "blue", tag:"W" },
     { name: "Server", command: "npm run start --prefix server", color: "green", tag:"S" },
+    { name: "Server (nodemon)", command: "npm run nodemon --prefix server", color: "lightblue", tag:"SN" },
     { name: "WebApp", command: "npm run start --prefix webapp", color: "yellow", tag:"WA" },
     { name: "Mobile", command: "npm run start --prefix mobile", color: "red", tag:"M" }
     //{ name: "Abrir Navegador"}
@@ -21,12 +23,19 @@ const checkboxOptions = services.map(service => ({
     }
 }));
 
-const answer = await checkbox({
+let answer = await checkbox({
     message: "Quais serviços você deseja iniciar?",
     choices: checkboxOptions,
     required: true,
     loop: true,
 });
+
+// Roda apenas o nodemon se o usuarios escolheu server e nodemon
+if (answer.some(s => s.name === 'S') && answer.some(s => s.name === 'SN')) {
+    answer = answer.filter(s => s.name !== 'S');
+    console.log(chalk.yellow.bold("Você escolheu iniciar o servidor com nodemon. O comando 'Server' não será iniciado."));
+    
+}
 
 const result = concurrently(answer);
 
